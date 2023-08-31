c1c389fe5fb5219574ebad46e881c1e20533a528
mariuspentz <82095559+mariuspentz@users.noreply.github.com>
2023-08-31 18:24:21 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,20 @@
-/* Javascript Code Ressource URL */
+<script> /* Javascript Code Ressource URL 
 var URL = "https://mc2xsctbllc5pw-0ck06pnh5c6g8.pub.sfmc-content.com/falan01rhrt";
+*/
+console.log("start js-code ressource"); 
+
+(function (/* root, doc */) {
+
+ // add your code here...
+var a = "I am a String";
+var b = "me too!";
+
+var c = a + ' ' + b;
+
+console.log("why am I a function?");
+console.log(c);
+console.log("can u see the var 'c' ?");
+}(window, document));
+console.log("do I print out window document??"); 
+console.log("end js-code ressource"); 
+</script>
