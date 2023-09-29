b72dc82960a0c2e4a43939f6df04d7121cde3375
mariuspentz <pentzmarius@gmail.com>
2023-09-29 09:45:22 +0200

    asdasd

--- test.js
+++ test.js
@@ -1,25 +1,3 @@
-<script> /* Javascript Code Ressource URL 
-var URL = "https://mc2xsctbllc5pw-0ck06pnh5c6g8.pub.sfmc-content.com/falan01rhrt";
-*/
-/*console.log("start js-code ressource"); 
-
-(function (/* root, doc */) {
-
- // add your code here...
-var a = "I am a String";
-var b = "me too!";
-
-var c = a + ' ' + b;
-
-console.log("why am I a function?");
-console.log(c);
-console.log("can u see the var 'c' ?");
-}(window, document));
-console.log("do I print out window document??"); 
-console.log("end js-code ressource"); */
-</script>
-<!-- <html>123</html -->
-
 import './App.css';
 import Header from './components/header/Header'; // Import the Header component
 import Footer from './components/footer/Footer'; // Import the Header component
