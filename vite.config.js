e29c8510d76a2e0b00ff40814d99c3f19559e975
Marius Pentz <pentzmarius@gmail.com>
2026-04-21 18:24:29 +0200

    react

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './test.html',
+      output: {
+        entryFileNames: 'bundle.js',
+        assetFileNames: 'bundle.[ext]',
+      },
+    },
+    assetsInlineLimit: 0, // Inline all assets
+  },
+});
